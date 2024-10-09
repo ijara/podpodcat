9e4be65aa1720be3f5efda1e3fae815444c6c859
Ignacio Jara <ijara756@gmail.com>
2024-10-09 12:53:32 -0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # podpodcat
+## commands
+- commands
+	- sudo apt-get update
+	- sudo apt-get install  python3 python3-pip ffmpeg
+    - sudo apt install python3.12-venv
+    - python3 -m venv .venv
+	- source .venv/bin/activate
+    - pip install podcastfy
+	- pip freeze > requirements.txt
+	- pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+import re
+
+
+def scrape_page(url):
+    # Realizar la solicitud HTTP
+    response = requests.get(url)
+
+    # Verificar si la solicitud fue exitosa (código de estado 200)
+    if response.status_code == 200:
+        # Verificar si el acceso está permitido según robots.txt
+        if is_allowed_by_robots(url):
+            # Parsear el contenido HTML de la página con BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
+            links = soup.find_all('a', href=lambda href: href and 'MLC' in href)
+             # Mostrar el contenido de links
+            print("Enlaces encontrados:")
+            for link in links:
+                print(link.get('href'))
+        else:
+            print(f'Acceso no permitido por robots.txt para {url}')
+
+    else:
+        print(f'Error al hacer la solicitud HTTP. Código de estado: {response.status_code}')
+
+def is_allowed_by_robots(url):
+    # Get the base URL
+    base_url = urlparse(url).scheme + '://' + urlparse(url).netloc
+
+    # Get the robots.txt URL
+    robots_url = urljoin(base_url, '/robots.txt')
+
+    try:
+        # Fetch the robots.txt content
+        robots_content = requests.get(robots_url).text
+
+        # Check if the User-agent is allowed to access the given URL
+        return 'User-agent: *\nDisallow:' not in robots_content
+
+    except requests.exceptions.RequestException:
+        # If there is an error fetching robots.txt, assume it's allowed
+        return True
+    
+# URL de la página que quieres hacer scraping
+url = 'https://www.diariooficial.interior.gob.cl/edicionelectronica/index.php?date=09-10-2024&edition=43969'
+
+if is_allowed_by_robots(url):
+    scrape_page(url)
+else:
+    print(f'Acceso no permitido por robots.txt para {url}')
+
+
+    
