b0016525594b9d366c685668e7b9fb7526486173
Ignacio Jara <ijara756@gmail.com>
2024-11-12 22:46:00 -0300

    typi /

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Iniciando sincronización..." >> /home/ijara756/podpodcat//home/ijara756/podpodcat/output.txt
+echo "Iniciando sincronización..." >> /home/ijara756/podpodcat/home/ijara756/podpodcat/output.txt
 date >> /home/ijara756/podpodcat/output.txt
 git pull >> /home/ijara756/podpodcat/output.txt 2>&1
 echo "Activando entorno virtual..." >> /home/ijara756/podpodcat/output.txt
