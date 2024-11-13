5e8b3bb6589684f72a6e8af96575debe935b7d0a
Ignacio Jara <ijara756@gmail.com>
2024-11-12 22:25:13 -0300

    added git pull

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-*/1 * * * * /bin/sh -c 'source .venv/bin/activate && python main.py && git add . && git commit -m "sync_$(date +\'%Y%m%d\')" && git push'
+*/1 * * * * /bin/sh -c 'git pull && source .venv/bin/activate && python main.py && git add . && git commit -m "sync_$(date +\'%Y%m%d\')" && git push'
